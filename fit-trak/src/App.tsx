a598ca693593d0a8dd018c67bfa158944e519367
Antonio Yanchev <antonio.yanchev@gmail.com>
2024-11-15 13:42:02 +0000

    changes

--- fit-trak/src/App.tsx
+++ fit-trak/src/App.tsx
@@ -68,7 +68,7 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
+            <IonIcon aria-hidden="true" icon={ellipse} />
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
@@ -76,7 +76,7 @@ const App: React.FC = () => (
             <IonLabel>Tab 2</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
+            <IonIcon aria-hidden="true" icon={ellipse} />
             <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
         </IonTabBar>
